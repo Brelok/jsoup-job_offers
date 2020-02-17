dda1de388a77f923e79b7aa20bf07f60ae893829
Brelok <code2pradelok@gmail.com>
2020-02-17 18:35:45 +0100

    add Bulldogsjob portal

--- src/brelok/workshop/LinkFIlter.java
+++ src/brelok/workshop/LinkFIlter.java
@@ -17,22 +17,79 @@ import java.util.stream.Collectors;
 public class LinkFIlter {
 
     final static String URL_JJO = "https://www.juniorjobsonly.com/jobs?page=";
+    final static String URL_BJ = "https://bulldogjob.pl/companies/jobs?page=";
     private static Path path = Paths.get("/Users/brelok/workspace/jsoup junior_jobs_only/offers.txt");
+    final static String javascript = "Javascript";
 
     public static void main(String[] args) {
 
-        List<String> juniorJobsOnly = getLinks("Wrocław", "java");
+
+        List<String> juniorJobsOnly = getLinks_JJO("Wrocław", "java");
+
+        List<String> bulldogJobs = getLinks_BJ("Wrocław", "java");
+
+        List<String> allPortals = new ArrayList<>();
+        allPortals.addAll(juniorJobsOnly);
+        allPortals.addAll(bulldogJobs);
 
         try {
-            Files.write(path, juniorJobsOnly);
+            Files.write(path, allPortals);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
+    private static List<String> getLinks_BJ(String city, String filter) {
+        List<String> list = new ArrayList<>();
 
-    private static List<String> getLinks(String city, String filter) {
+        for (int i = 1; i <= 7; i++) {
+            List<String> href = new ArrayList<>();
+
+            try {
+                Connection.Response homePage = Jsoup.connect(URL_BJ + i)
+                        .method(Connection.Method.GET)
+                        .execute();
+
+                Document document = homePage.parse();
+
+                Elements elementsLi = document.select("body > .main-content > .app-content.listing > " +
+                        ".container > #search-result > .col-sm-10.col-sm-offset-1 > " +
+                        ".search-results > .results-list.list-unstyled.content > li");
+
+                for (Element e : elementsLi) {
+                    if (!e.is(".results-list-item.subscribe-search")) {
+
+                        if (e.child(0).child(1).child(0).select(".pop-mobile").text().contains(city)) {
+                            href.add(e.child(0).child(0).child(0).attr("href"));
+                        }
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            list.addAll(href);
+        }
+
+        List<String> filtredList = list.stream()
+                .map(String::toLowerCase)
+                .filter(s -> s.contains(filter.toLowerCase()))
+                .filter(s -> !s.contains(javascript.toLowerCase()))
+                .collect(Collectors.toList());
+
+        List<String> notFoundList = new ArrayList<>();
+        notFoundList.add("Nic nie znaleziono na BulldogsJobs, zmień parametry");
+
+        if (filtredList.size() < 1) return notFoundList;
+        else {
+            return filtredList;
+        }
+    }
+
+
+    private static List<String> getLinks_JJO(String city, String filter) {
 
         List<String> list = new ArrayList<>();
 
@@ -62,14 +119,16 @@ public class LinkFIlter {
         List<String> filtredList = list.stream()
                 .map(String::toLowerCase)
                 .filter(s -> s.contains(filter.toLowerCase()))
+                .filter(s -> !s.contains(javascript.toLowerCase()))
                 .collect(Collectors.toList());
 
         List<String> notFoundList = new ArrayList<>();
-        notFoundList.add("Nic nie znaleziono, zmień parametry");
+        notFoundList.add("Nic nie znaleziono na Junior Jobs Only, zmień parametry");
 
         if (filtredList.size() < 1) return notFoundList;
         else {
             return filtredList;
         }
     }
+
 }
